efc91612cf66910e6e2bdf8aa6ef6cdb579fad8f
Krisha-Dhakal <85564341+Krisha-Dhakal@users.noreply.github.com>
2024-04-29 11:44:04 +0545

    Add files via upload

---
+++ WebApp1ByKrisha/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApp1ByKrisha.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
