739192afdad45de4c0829270ef75b86831dda7cc
Krisha-Dhakal <85564341+Krisha-Dhakal@users.noreply.github.com>
2024-04-29 11:50:06 +0545

    Add files via upload

---
+++ WebApp2ByKrisha/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApp2ByKrisha.Models;
+using WebApp2ByKrisha.Services;
+
+namespace WebApp2ByKrisha.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IProductService _productService;
+        public HomeController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public IActionResult Index()
+        {
+            return View(_productService.GetAll());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ WebApp2ByKrisha/Models/ProductViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApp2ByKrisha.Models
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
