4d7cd588879507995745e3f1a8d1bba3f1d4d7dd
Krisha-Dhakal <85564341+Krisha-Dhakal@users.noreply.github.com>
2024-04-29 11:42:58 +0545

    Add files via upload

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebApp1ByKrisha.Models;
+
+namespace WebApp1ByKrisha.Controllers
+{
+    public class StudentController: Controller
+    {
+        public IActionResult MyRazorPage()
+        {
+            return View();
+        }
+        public IActionResult CreateStudent()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateStudent(Student s) 
+        {
+            if (ModelState.IsValid)
+            {
+                return View("StudentDetail", s);
+            }
+            else
+            {
+                return View(s);
+            }
+        }
+        public IActionResult StudentDetail(Student s)
+        {
+            return View(s);
+        }
+    }
+}
