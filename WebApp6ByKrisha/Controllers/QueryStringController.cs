0cc385b0652ee4e7b755869c6ac724121595a84b
Krisha-Dhakal <85564341+Krisha-Dhakal@users.noreply.github.com>
2024-05-28 22:44:43 +0545

    Add files via upload

---
+++ WebApp6ByKrisha/Controllers/QueryStringController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp6ByKrisha.Models;
+
+namespace WebApp6ByKrisha.Controllers
+{
+    public class QueryStringController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetQueryString(string name, int age)
+        {
+            User newUser = new User()
+            {
+                Name = name,
+                Age = age
+            };
+            return View(newUser);
+        }
+    }
+}
---
+++ WebApp6ByKrisha/Controllers/TempDataController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp6ByKrisha.Controllers
+{
+    public class TempDataController : Controller
+    {
+        public IActionResult Index()
+        {
+            TempData["myMessage"] = "Greetings, This is temp data message.";
+            return View();
+        }
+        public IActionResult TempDataPage()
+        {
+            return View();
+        }
+    }
+}
